f53e55e1722691a3df4f32046bde52923389b459
Paweł Marcinek <pawel.marcinek@intive.com>
2018-03-14 14:26:30 +0100

    counter commit v2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 var counter = 0
-app.get('/', (req, res) => res.send('Hello World! This Michal`s version. Number of visits is ' + ++counter))
+app.get('/', (req, res) => res.send('Hello World! This Pawel Marcinek version. Number of visits is ' + ++counter))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
